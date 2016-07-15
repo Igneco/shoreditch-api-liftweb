c7cf376bdf906a5e1fceaef53432734993533fae
alltonp <paulallton@gmail.com>
2016-07-15 19:22:45 +0100

    add handle to liftweb api

--- src/main/scala/im/mange/shoreditch/api/liftweb/LiftwebToShoreditchRequestAdaptor.scala
+++ src/main/scala/im/mange/shoreditch/api/liftweb/LiftwebToShoreditchRequestAdaptor.scala
@@ -1,14 +1,14 @@
 package im.mange.shoreditch.api.liftweb
 
+import im.mange.shoreditch.Shoreditch
 import im.mange.shoreditch.api.Request
 import net.liftweb.common.Full
-import net.liftweb.http.Req
+import net.liftweb.http.{JsonResponse, LiftResponse, PlainTextResponse, Req}
 import net.liftweb.json.Serialization._
 import net.liftweb.json._
 
 //TODO: and put the response stuff n here too ... so as little code as possible ...
 case class LiftwebToShoreditchRequestAdaptor(req: Req) extends Request {
-
   val path = req match {
     case Req(x, _, _) ⇒ x.mkString("/")
     case _ ⇒ ""
@@ -23,22 +23,15 @@ case class LiftwebToShoreditchRequestAdaptor(req: Req) extends Request {
     case _ => ""
   }
 
+  def handle(shoreditch: Shoreditch) = {
+    val result = shoreditch.handle(this)
+
+    val response: LiftResponse = result match {
+      case None => PlainTextResponse("Nothing to see here")
+      case Some(json) => JsonResponse(parse(json))
+    }
+    Full(response)
+  }
+
   override def toString = s"$path => ${json}"
-
-//  def handle(shoreditch: Shoreditch) = {
-//    val handler = Booking.handler(request)
-//    //      val handler = Cleared.handler(new LiftwebRequest(req))
-//    println(handler)
-//
-//    val response: LiftResponse = handler match {
-//      case None => PlainTextResponse("Nothing to see here")
-//      case Some(j) => JsonResponse(parse(j()))
-//    }
-//    Full(response)
-//  }
-
 }
-
-
-
-
