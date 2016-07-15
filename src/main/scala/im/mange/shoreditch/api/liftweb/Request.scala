c96f48a8f2e390676d731cfb09a11c596d45f856
alltonp <paulallton@gmail.com>
2016-07-15 13:06:00 +0100

    a little get or elsing ...

--- src/main/scala/im/mange/shoreditch/api/liftweb/Request.scala
+++ src/main/scala/im/mange/shoreditch/api/liftweb/Request.scala
@@ -12,7 +12,7 @@ trait Request {
 }
 
 case class LiftwebRequest(req: Req) extends Request {
-  val json = if (req.json_?) req.json.get else req.forcedBodyAsJson.get
+  val json = if (req.json_?) req.json.getOrElse("") else req.forcedBodyAsJson..getOrElse("")
 
   val inboundPathParts = req match {
     case Req(x, _, _) ⇒ x
