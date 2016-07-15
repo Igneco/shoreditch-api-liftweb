af593167292120f6b6da3fea62e8faae8b3aa9d9
alltonp <paulallton@gmail.com>
2016-07-15 19:18:24 +0100

    spring cleaning

--- src/main/scala/im/mange/shoreditch/api/Runner.scala
+++ src/main/scala/im/mange/shoreditch/api/Runner.scala
@@ -3,21 +3,11 @@ package im.mange.shoreditch.api
 object Runner {
   import net.liftweb.json._
 
-  //TODO: this needs the post'ed input
-  def run(a: Action, req: Request) = {
-    //TODO: we should definitely validate the json in our regular way here ...
-    val r = try {
-      val in = Json.deserialiseIn(parse(req.json))
-      a.run(in)
-    }
+  def run(a: Action, req: Request) =
+    try { a.run(Json.deserialiseIn(parse(req.json))) }
     catch { case e: Throwable ⇒ a.failure(List(e.getMessage)) }
-    r
-  }
 
-  def run(c: Check) = {
-    val r = try { c.run }
+  def run(c: Check) =
+    try { c.run }
     catch { case e: Throwable ⇒ c.failure(List(e.getMessage)) }
-    r
-  }
-
 }
