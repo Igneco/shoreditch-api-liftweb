5ec1ecd9a664f15e2ce3b9203e1e516078918fc8
alltonp <paulallton@gmail.com>
2016-07-15 19:05:38 +0100

    clean api

--- src/test/scala/im/mange/shoreditch/example/SimpleRequest.scala
+++ src/test/scala/im/mange/shoreditch/example/SimpleRequest.scala
@@ -2,4 +2,4 @@ package im.mange.shoreditch.example
 
 import im.mange.shoreditch.api.Request
 
-case class SimpleRequest(json: String, inboundPathParts: Seq[String]) extends Request
+case class SimpleRequest(inboundPathParts: Seq[String], json: String = "") extends Request
