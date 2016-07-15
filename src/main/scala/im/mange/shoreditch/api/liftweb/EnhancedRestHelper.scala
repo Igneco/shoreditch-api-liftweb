14eedd61907ee1f6297afb042dc7ad4adc68b74b
alltonp <paulallton@gmail.com>
2016-07-15 15:39:42 +0100

    folding heirarchy

--- src/main/scala/im/mange/shoreditch/api/liftweb/EnhancedRestHelper.scala
+++ src/main/scala/im/mange/shoreditch/api/liftweb/EnhancedRestHelper.scala
@@ -9,7 +9,7 @@ object EnhancedRestHelper {
   case class StaticPathPart(str: String) extends PathPart { def simpleString = str }
   case class DynPathPart(name: String) extends PathPart { def simpleString = "@" + name }
 
-  private def splitPath(str: String): List[PathPart] = {
+  def splitPath(str: String): List[PathPart] = {
     val pathParts: Array[PathPart] = str split '/' map {
       case x if x startsWith "@" ⇒ DynPathPart(x.tail)
       case x ⇒ StaticPathPart(x)
@@ -79,45 +79,4 @@ object EnhancedRestHelper {
 import im.mange.shoreditch.api.liftweb.EnhancedRestHelper._
 
 abstract class EnhancedRestHelper[Service](longName: String = "", alias: String = "", base: String = "", summary: String = "", version: String)(routes: Route[Service]*) {
-  val shoreditch = Shoreditch(base, version, longName, alias, summary, routes)
-
-  type ShoreditchResponse = () ⇒ String
-
-  private val basePathParts = splitPath(base)
-
-  def xform(req: Request): Service ⇒ ShoreditchResponse
-
-  private val rebasedRoutes: Seq[Route[Service]] = routes.map { _ withBase basePathParts }
-
-  //TODO: two things in here might explain the bogus GET listings we get ...
-  private def summaryHandler(req: Request): Option[ShoreditchResponse] = {
-    //TODO: not sure about this check actually ...
-    if(summary.isEmpty) None
-    else {
-      val summaryResponse: ShoreditchResponse = () => {
-        val theActions = shoreditch.actions.map(a => ActionMetaData(a._1, a._2.parameters.in, a._2.parameters.out)).toList
-        val theChecks = shoreditch.checks.map(c => CheckMetaData(c._1)).toList
-
-        val metaData = MetaDataResponse(longName, alias, version, theChecks, theActions)
-        Json.serialise(metaData)
-      }
-      val summaryRoute: Route[ShoreditchResponse] = GET0(summary) {
-        summaryResponse
-      } withBase basePathParts
-      summaryRoute.attemptMatch(req)
-    }
-  }
-
-  private val matchers: Seq[(Request) => Option[Service]] = rebasedRoutes map { r ⇒ r.attemptMatch _ }
-
-  private def lazyAppliedMatches(req: Request) = matchers.iterator map { _(req) }
-  private def firstMatchingRoute(req: Request) = lazyAppliedMatches(req).find(_.isDefined).flatten
-
-  //TODO: checks need to be GET and actions need to be POST, but how if only 1 endpoint ...
-  //TODO: we really want shoreditch to mount as one endpoint ...
-  //TODO: could do post with a Cmd section? are we getting too far away from things?
-  //TODO: that would be annoying for reprobabte which really wants to do queries
-  //TODO: could seperate into two different endpoints checks - GET and actions - POST
-  def handler(req: Request) : Option[ShoreditchResponse] =
-    firstMatchingRoute(req).map(xform(req)) orElse summaryHandler(req)
 }
--- src/main/scala/im/mange/shoreditch/api/liftweb/ServiceHelper.scala
+++ src/main/scala/im/mange/shoreditch/api/liftweb/ServiceHelper.scala
@@ -1,5 +1,6 @@
 package im.mange.shoreditch.api.liftweb
 
+import im.mange.shoreditch.Shoreditch
 import im.mange.shoreditch.api._
 import im.mange.shoreditch.api.liftweb.EnhancedRestHelper._
 //import net.liftweb.http.{JsonResponse, LiftResponse, Req}
@@ -21,8 +22,51 @@ object ServiceHelper {
 }
 
 //TODO: ultimately rename me ...
-abstract class ServiceHelper(longName: String, alias: String, base: String, version: String, checksEnabled: Boolean, actionsEnabled: Boolean)(offerings: Route[Service]*)
-  extends EnhancedRestHelper[Service](longName, alias, base, "metadata", version)(offerings: _*) {
+abstract class ServiceHelper(longName: String, alias: String, base: String, version: String, checksEnabled: Boolean, actionsEnabled: Boolean)(routes: Route[Service]*)
+  extends EnhancedRestHelper[Service](longName, alias, base, "metadata", version)(routes: _*) {
+
+  val shoreditch = Shoreditch(base, version, longName, alias, "metadata", routes)
+
+  type ShoreditchResponse = () ⇒ String
+
+  private val basePathParts = splitPath(base)
+
+//  def xform(req: Request): Service ⇒ ShoreditchResponse
+
+  private val rebasedRoutes: Seq[Route[Service]] = routes.map { _ withBase basePathParts }
+
+  //TODO: two things in here might explain the bogus GET listings we get ...
+  private def summaryHandler(req: Request): Option[ShoreditchResponse] = {
+    //TODO: not sure about this check actually ... it's stupid, remove it ...
+    if(shoreditch.summary.isEmpty) None
+    else {
+      val summaryResponse: ShoreditchResponse = () => {
+        val theActions = shoreditch.actions.map(a => ActionMetaData(a._1, a._2.parameters.in, a._2.parameters.out)).toList
+        val theChecks = shoreditch.checks.map(c => CheckMetaData(c._1)).toList
+
+        val metaData = MetaDataResponse(longName, alias, version, theChecks, theActions)
+        Json.serialise(metaData)
+      }
+      val summaryRoute: Route[ShoreditchResponse] = GET0(shoreditch.summary) {
+        summaryResponse
+      } withBase basePathParts
+      summaryRoute.attemptMatch(req)
+    }
+  }
+
+  private val matchers: Seq[(Request) => Option[Service]] = rebasedRoutes map { r ⇒ r.attemptMatch _ }
+
+  private def lazyAppliedMatches(req: Request) = matchers.iterator map { _(req) }
+  private def firstMatchingRoute(req: Request) = lazyAppliedMatches(req).find(_.isDefined).flatten
+
+  //TODO: checks need to be GET and actions need to be POST, but how if only 1 endpoint ...
+  //TODO: we really want shoreditch to mount as one endpoint ...
+  //TODO: could do post with a Cmd section? are we getting too far away from things?
+  //TODO: that would be annoying for reprobabte which really wants to do queries
+  //TODO: could seperate into two different endpoints checks - GET and actions - POST
+  def handler(req: Request) : Option[ShoreditchResponse] =
+    firstMatchingRoute(req).map(xform(req)) orElse summaryHandler(req)
+
 
   def xform(req: Request): (Service) => () => String = mkRunFunc(_, req)
 
