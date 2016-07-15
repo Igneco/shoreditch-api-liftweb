5ead369fdfa2b17f248a7a0b29a934680bb4a658
alltonp <paulallton@gmail.com>
2016-07-15 16:05:45 +0100

    pass our booleans through to shoreditch

--- src/main/scala/im/mange/shoreditch/Shoreditch.scala
+++ src/main/scala/im/mange/shoreditch/Shoreditch.scala
@@ -6,4 +6,6 @@ import im.mange.shoreditch.api.liftweb.EnhancedRestHelper._
 //TODO: should be no api.liftwen deps in here
 //TODO: see if we can whack summary
 //TODO: decide what is mandatory and use Options
-case class Shoreditch[Service](base: String, version: String, longName: String = "", alias: String = "", routes: Seq[Route[Service]])
+case class Shoreditch[Service](base: String, version: String, longName: String = "", alias: String = "",
+                               checksEnabled: Boolean, actionsEnabled: Boolean,
+                               routes: Seq[Route[Service]])
